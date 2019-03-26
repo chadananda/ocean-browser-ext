2ce61601b4c21b22d9c44a98226abc9e24329aa8
javascriptninja815 <xandokan815@protonmail.com>
2019-03-26 01:50:21 -0700

    -

--- popup/popup.js
+++ popup/popup.js
@@ -3,26 +3,40 @@
 var nodes = {
   openOceanSearch: document.getElementById('openOceanSearch'),
   language: document.getElementById('language'),
-  tools: {
-    key_mapping: document.getElementById(key_mapping),
-    auto_correct: document.getElementById(auto_correct),
-    spellcheck: document.getElementById(spellcheck)
-  },
+  tools: document.getElementById('tools'),
   download: document.getElementById('download')
 }
 const downloadLink = "https://sacred-traditions.org/ocean/";
 function setListeners() {
-  function openDownloadPage() {
-    chrome.tabs.create({ url: downloadLink });
-  }
   // function openOceanSearch(){
 
   // }
+  function openDownloadPage() {
+    chrome.tabs.create({ url: downloadLink });
+  }
+  function checkBoxHandler(handler){
+    if (handler.checked) {
+      console.log(handler.value)
+    } else {
+      console.log('unchecked')
+    }
+  }
+  nodes.tools.querySelector('input[name=key_mapping]').addEventListener('change', function(){
+    checkBoxHandler(this);
+  })
+  nodes.tools.querySelector('input[name=auto_correct]').addEventListener('change', function(){
+    checkBoxHandler(this);
+  })
+  nodes.tools.querySelector('input[name=spell_check]').addEventListener('change', function(){
+    checkBoxHandler(this);
+  })
   nodes.download.addEventListener('click', function(){
     openDownloadPage()
   })
+  
   // nodes.openOceanSearch.addEventListener('click', function(){
   //   openOceanSearch();
   // })
+
 }
 setListeners();
