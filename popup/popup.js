c4a8d21f00ce0a6b3dd9465a9ba0c95b4c40cb34
javascriptninja815 <xandokan815@protonmail.com>
2019-03-27 06:11:31 -0700

    -

--- popup/popup.js
+++ popup/popup.js
@@ -1,16 +1,51 @@
 "use strict";
 
 var nodes = {
-  openOceanSearch: document.getElementById('openOceanSearch'),
-  language: document.getElementById('language'),
+  openOceanSearch: document.getElementById('open_ocean_search'),
+  language: document.getElementById('language_letter'),
   tools: document.getElementById('tools'),
   download: document.getElementById('download')
 }
 const downloadLink = "https://sacred-traditions.org/ocean/";
+const locales = {
+  en: {
+    openOceanSearch: 'Ocean 2.0 Search, Alt-S',
+    language: 'Language',
+    editTool: 'Editing Tools',
+    keyMapping: 'Shortcut Characters',
+    autoCorrect: 'Auto-correct Common',
+    spellCheck: 'Spellcheck Terms',
+    download: 'Ocean 2.0 App'
+  },
+  ar: {
+    openOceanSearch: 'بحث المحيط 2.0 ، Alt-S',
+    language: 'لغة',
+    editTool: 'أدوات التحرير',
+    keyMapping: 'أحرف الاختصار',
+    autoCorrect: 'التصحيح التلقائي المشترك',
+    spellCheck: 'شروط التدقيق الإملائي',
+    download: 'محيط 2.0 التطبيق'
+  },
+  fa: {
+    openOceanSearch: 'اقیانوس 2.0 جستجو، Alt-S',
+    language: 'زبان',
+    editTool: 'ویرایش ابزارها',
+    keyMapping: 'شخصیت های میانبر',
+    autoCorrect: 'تصحیح خودکار رایج است',
+    spellCheck: 'اصطلاحات املا',
+    download: 'اقیانوس 2.0 برنامه'
+  }
+}
 function setListeners() {
-  // function openOceanSearch(){
+  function changePopupUI(lang) {
+    // nodes.openOceanSearch.innerHTML = locales[lang].openOceanSearch;
+    // nodes.language.innerHTML = locales[lang].language;
+    console.log(nodes.language.innerHTML);
+    console.log(locales[lang].language);
+  }
+  function openOceanSearch(){
 
-  // }
+  }
   function openDownloadPage() {
     chrome.tabs.create({ url: downloadLink });
   }
@@ -23,7 +58,9 @@ function setListeners() {
   }
   nodes.language.onchange = function() {
     var value = this.options[this.selectedIndex].value;
-    if (value == 'en') changePopup()
+    if (value == 'ar') changePopupUI('ar');
+    else if (value == 'fa') changePopupUI('fa');
+    else changePopupUI('en');
   }
   nodes.tools.querySelector('input[name=key_mapping]').addEventListener('change', function(){
     checkBoxHandler(this);
