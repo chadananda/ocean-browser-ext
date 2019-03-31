fb159b8e990e2a2a8f1b31a7c4a27dc691a64a53
javascriptninja815 <xandokan815@protonmail.com>
2019-03-31 11:08:51 -0700

    finished shortcut and language

--- content/insert_libs.js
+++ content/insert_libs.js
@@ -9,20 +9,18 @@ loadAndPopupSearch_Cached = function() {
   let script1 = 'https://unpkg.com/vue@2.6.10/dist/vue.js'
   let script2 = 'https://search-widget.current.build.ocean.isddesign.com/search-widget-ocean.min.js'
   $.cachedScript(script1).done(() => {
-    $.cachedScript(script2).done(() => {
-      $("body").append("<search-widget-ocean></search-widget-ocean>")
-    })
-  })
+    $.cachedScript(script2);
+  });
 }
 loadAndPopupSearch_Cached();
 // listen click event on extension
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.cmd) {
-    console.log(request.cmd);
+    if ($('search-widget-ocean')) $('search-widget-ocean').remove();
     if (request.cmd == 'toggle-feature-foo') {
-      $("search-widget-ocean").attr('language', 'en'); 
+      $("body").append("<search-widget-ocean language='en'></search-widget-ocean>");
     } else {
-      $("search-widget-ocean").attr('language', requst.cmd); 
+      $("body").append("<search-widget-ocean language=" + request.cmd + "></search-widget-ocean>");
     }
     $('search-widget-ocean')[0].shadowRoot.querySelector('#search-popup').click();
     sendResponse({ res: 'success' });
