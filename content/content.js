65adb82213168c8af49b2b8c3aec1f56a4ec1804
javascriptninja815 <xandokan815@protonmail.com>
2019-04-04 00:59:46 -0700

    get element by classname on google.com

--- content/content.js
+++ content/content.js
@@ -14,11 +14,11 @@ loadAndPopupSearch_Cached = function () {
 }
 
 // get url and detect
-insertOceanComponent = function () {
+insertOceanElement = function () {
   var url = window.location.href;
   switch (url) {
     case 'https://www.google.com/': {
-      var element = document.body.children[3].children[6].children[2].children[1].children[0].children[0].children[0];
+      var element = document.querySelector('.gb_Zd');
       var component = container({
         id: 'google',
         parentClass: element.childNodes[0].className,
@@ -30,20 +30,19 @@ insertOceanComponent = function () {
   }
 }
 
-
-
 loadAndPopupSearch_Cached();
-insertOceanComponent();
+insertOceanElement();
 // insert component when user clicks extension or press shortcut (CTRL+SHIFT+Y)
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.cmd) {
-    if ($('search-widget-ocean')) $('search-widget-ocean').remove();
-    $("body").append(
-      "<search-widget-ocean language=" +
-      request.cmd +
-      "></search-widget-ocean>"
-    );
-    $('search-widget-ocean')[0].shadowRoot.querySelector('#search-popup').click();
+    if (!$('search-widget-ocean').length) {
+      $("body").append(
+        "<search-widget-ocean language=" +
+        request.cmd +
+        "></search-widget-ocean>"
+      );
+    }
+    $('search-widget-ocean').shadowRoot.querySelector('#search-popup').click();
     sendResponse({ res: 'success' });
   }
   return true;
