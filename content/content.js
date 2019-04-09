d8e5473260b5b9e119c97174ec7d536cb9598d7c
javascriptninja815 <xandokan815@protonmail.com>
2019-04-09 03:02:11 -0700

    finished baidu.com

--- content/content.js
+++ content/content.js
@@ -110,6 +110,25 @@ insertOceanElement = function () {
         wrapper.appendChild(oceanComponent);
         element.appendChild(wrapper);
       })
+      break;
+    // case 'https://www.reddit.com/':
+    //   var oceanComponent = container({});
+    // case 'https://www.yahoo.com/':
+    case 'https://www.baidu.com/':
+      var oceanComponent = container({});
+
+      oceanComponent.style.position = 'absolute';
+      oceanComponent.style.top = '0px';
+      oceanComponent.style.margin = '20px 15px 5px';
+
+      oceanComponent.childNodes[1].style.color = '#000';
+      oceanComponent.childNodes[1].style.fontWeight = '700';
+
+      $(window).on('load', function () {
+        var element = document.querySelector('.head_wrapper');
+        element.appendChild(oceanComponent);
+      })
+      break;
   }
 }
 
