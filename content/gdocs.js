b9c7cb366522ad68c1e0de9ec760d26a494c1dac
javascriptninja815 <xandokan815@protonmail.com>
2019-05-15 00:02:11 -0700

    insert text into gdocs but didn't move caret

---
+++ content/gdocs.js
@@ -0,0 +1,40 @@
+$(window).on('load', function () {
+  var port = chrome.runtime.connect({
+    name: 'ocean extension'
+  })
+  port.postMessage({
+    method: 'getEditToolSetting'
+  })
+  port.onMessage.addListener(function (msg) {
+    if (msg.response == 'editToolSetting') {
+      if (msg.data.keyMapping) {
+        // function insertCharacter(text) {
+        //   googleDocsUtil.setTextCurrentCursor(text);
+        //   // var googleDoc = googleDocsUtil.getGoogleDocument();
+        // }
+        var editingIframe;
+        var findEditingIframe = setInterval(function () {
+          editingIframe = document.getElementsByTagName('iframe')[4];
+          console.log('...finding editIframe...');
+          if (editingIframe) {
+            console.log('found');
+            Mousetrap(editingIframe.contentDocument).bind("alt+. h", function (e) {
+              e.preventDefault();
+              console.log('called');
+              googleDocsUtil.setTextCurrentCursor("ḥ");
+            })
+            // Mousetrap(editingIframe).bind({
+            //   "alt+. h": function(e) {
+            //     e.preventDefault();
+            //     console.log('called');
+            //     // insertCharacter("ḥ");
+            //     googleDocsUtil.setTextCurrentCursor("ḥ");
+            //   }
+            // })
+            clearInterval(findEditingIframe);
+          }
+        }, 2000);
+      }
+    }
+  })
+})
